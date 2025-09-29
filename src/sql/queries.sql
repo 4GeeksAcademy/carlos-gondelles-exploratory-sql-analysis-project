0bbf5140be6d2c989b31029b5a4e8aef544d9063
gondelles <carlosluis.gondelles@gmail.com>
2025-09-29 19:39:00 +0000

    Carlos Gondelles Exploratory SQL Analysis Project

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -22,31 +22,73 @@ SELECT * FROM observations;
 
 
 -- MISSION 1
--- Your query here;
+--¿Cuáles son las primeras 10 observaciones registradas?;
+SELECT * from climate limit 10;
 
 -- MISSION 2
--- Your query here;
-
+-- ¿Qué identificadores de región (region_id) aparecen en los datos?;
+SELECT "region_id" from  regions;
 
 -- MISSION 3
--- Your query here;
-
+-- ¿Cuántas especies distintas (species_id) se han observado?;
+SELECT DISTINCT COUNT("species_id") from species;
 
 -- MISSION 4
--- Your query here;
-
+-- ¿Cuántas observaciones hay para la región con region_id = 2?;
+SELECT COUNT("region_id")
+from observations
+WHERE "region_id" = 2;
 
 -- MISSION 5
--- Your query here;
-
+-- ¿Cuántas observaciones se registraron el día 1998-08-08?;
+SELECT *
+from observations
+WHERE "observation_date" = "1998-08-08";
 
 -- MISSION 6
--- Your query here;
-
+-- ¿Cuál es el region_id con más observaciones?;
+SELECT COUNT("region_id")
+from observations
+group by region_id;
 
 -- MISSION 7
--- Your query here;
-
+-- ¿Cuáles son los 5 species_id más frecuentes?;
+SELECT *
+from observations
+GROUP BY species_id
+ORDER BY species_id DESC
+LIMIT 5;
 
 -- MISSION 8
--- Your query here;
+-- ¿Qué especies (species_id) tienen menos de 5 registros?;
+SELECT *
+FROM observations
+HAVING 
+
+
+-- MISSION 9
+-- ¿Qué observadores (observer) registraron más observaciones?;
+
+
+-- MISSION 10
+-- Muestra el nombre de la región (regions.name) para cada observación.?;
+
+
+-- MISSION 11
+-- Muestra el nombre científico de cada especie registrada (species.scientific_name);
+
+
+-- MISSION 12
+-- ¿Cuál es la especie más observada por cada región?;
+
+
+-- MISSION 13
+-- Inserta una nueva observación ficticia en la tabla observations;
+
+
+-- MISSION 14
+-- Corrige el nombre científico de una especie con error tipográfico;
+
+
+-- MISSION 15
+-- Elimina una observación de prueba (usa su id);
